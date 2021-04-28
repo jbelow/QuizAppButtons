f8643f550ea94109180089bf0c40ed0b21c4f1b0
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-04-27 21:16:56 -0500

    Initial commit

---
+++ QuizAppButtons/QuizAppButtons/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace QuizAppButtons
+{
+    public partial class MainPage : ContentPage
+    {
+
+        IList<QuizQuestionsViewModel> questions = new List<QuizQuestionsViewModel>();
+        private int index;
+        private int correct;
+        private int wrong;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void btnStartClicked(object sender, EventArgs args)
+        {
+            questions = QuizQuestionsViewModel.All;
+            index = 0;
+            theLabel.Text = questions[index].Title;
+            theImage.Source = questions[index].Image;
+            btnStart.IsVisible = false;
+            btnTrue.IsVisible = true;
+            btnFalse.IsVisible = true;
+        }
+
+        void btnTrueClicked(object sender, EventArgs args)
+        {
+            calculate(true);
+        }
+
+        void btnFalseClicked(object sender, EventArgs args)
+        {
+            calculate(false);
+        }
+
+
+        void calculate(bool btnAnswer)
+        {
+            if (index < questions.Count)
+            {
+                //theLabel.Text = e.Direction.ToString() + " " + index;
+                
+                if (questions[index].Answer == btnAnswer)
+                {
+                    correct++;
+                }
+                else
+                {
+                    wrong++;
+                }
+                
+
+            }
+
+            if (index >= questions.Count - 1)
+            {
+                btnTrue.IsVisible = false;
+                btnFalse.IsVisible = false;
+
+                theLabel.Text = "You Answered: " + correct + " correct" + " / Wrong: " + wrong;
+                theImage.Source = "finished.png";
+                //this is for the if check on line 47
+                index++;
+
+
+            }
+            else
+            {
+                index++;
+                theLabel.Text = questions[index].Title;
+                theImage.Source = questions[index].Image;
+            }
+        }
+    }
+}
---
+++ QuizAppButtons/QuizAppButtons/QuizQuestionsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizAppButtons
+{
+    public class QuizQuestionsViewModel
+    {
+
+
+        public QuizQuestionsViewModel(string title, bool answer, string image)
+        {
+
+            Title = title;
+            Answer = answer;
+            Image = image;
+        }
+
+        public string Title { private set; get; }
+
+        public bool Answer { private set; get; }
+
+        public string Image { private set; get; }
+
+        static QuizQuestionsViewModel()
+        {
+            All = new List<QuizQuestionsViewModel>
+            {
+
+                new QuizQuestionsViewModel("Are Husky puppies the cutest ever?", true, "puppy.jpg"),
+
+                new QuizQuestionsViewModel("Is this a good question?", false, "questionmark.jpg"),
+
+                new QuizQuestionsViewModel("Did Joshua Below write this code?", true, "josh.png"),
+
+                new QuizQuestionsViewModel("is this a quiz?", true, "quiz.jpg"),
+
+                new QuizQuestionsViewModel("Is there 28 hours in a day?", false, "hours.jpg"),
+
+
+            };
+        }
+
+        public static IList<QuizQuestionsViewModel> All { private set; get; }
+
+
+    }
+
+}
